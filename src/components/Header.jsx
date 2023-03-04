42f6540729a163e49e201eb2fd07fbd57aee54cd
Ivan Kurski <iskurskiii@gmail.com>
2023-03-04 18:55:01 +0300

    ADD: Search.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logoPizza from '../assets/img/pizza-logo.svg'
+import Search from './Search/Search'
 
-function Header() {
+function Header({ searchValue, setSearchValue }) {
 	return (
 		<div className='header'>
 			<div className='container'>
@@ -13,6 +14,7 @@ function Header() {
 						<p>самая вкусная пицца во вселенной</p>
 					</div>
 				</Link>
+				<Search searchValue={searchValue} setSearchValue={setSearchValue} />
 				<div className='header__cart'>
 					<Link to='/cart' className='button button--cart'>
 						<span>520 ₽</span>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NotFoundBlock from './NotFoundBlock/NotFoundBlock'
+import NotFoundBlock from '../components/NotFoundBlock/NotFoundBlock'
 
 const NotFound = () => {
 	return (
